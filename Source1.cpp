3dc5f496099b72db2c1522c735f753a1b742aff3
Tobste <36802409+Tobste@users.noreply.github.com>
2020-08-10 15:38:06 +0300

    Update Source1.cpp

--- Source1.cpp
+++ Source1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 class Matrix
 {
 public:
-//Это непонятная строка для проверки коммита создателяАААААААААААА
+//Это непонятная строка для проверки коммита создателяАААААААААААА2
 	int razm_str;
 	int razm_stolb;
 	int** Table;
